5a0a6224aeb4c7390fadf933a726fe3b80b88db3
Lasath Fernando <kde@lasath.org>
2014-09-16 22:06:54 +1000

    Redo everything with lambdas
    Holy shite they're so nice!!!

--- MinHS/Evaluator.hs
+++ MinHS/Evaluator.hs
@@ -42,6 +42,7 @@ evalE g (Num i) = I i
 evalE g (Con "True") = B True
 evalE g (Con "False") = B False
 evalE g (Con "Nil") = Nil
+evalE g (Con "Cons") = PApp $ PVal $ \(I i) -> PApp $ PVal $ \v -> Cons i v
 
 -- Variable lookup
 evalE g (Var id) = case E.lookup g id of
@@ -52,23 +53,18 @@ evalE g (Let [bind] exp) = evalE (boundEnv bind) exp
     where
         boundEnv (Bind id _ _ exp) = E.add g (id, (evalE g exp))
 
--- Function binding
---evalE g (Letfun b@(Bind id _ args exp)) = Closure (E.add g (id, (Closure g b))) b
-evalE g (Letfun b@(Bind id _ args exp)) = Closure g b
-
 -- IfthenElse blocks
 evalE g (If e1 e2 e3) = case evalE g e1 of
                             B True -> evalE g e2
                             B False -> evalE g e3
 
--- Function application
-evalE g (App (Var f) exp) = case E.lookup g f of
-        Nothing -> error $ "Error: function '" ++ show f ++ "' not in scope"
-        Just c@(Closure context b@(Bind id _ (arg:args) fexp)) -> evalE (E.addAll context [(arg, (evalE g exp)), (id, c)]) fexp
-evalE g (App (Letfun b@(Bind id _ [arg] fexp)) exp) = evalE (E.addAll g [(arg, (evalE g exp)), (id, (Closure g b))]) fexp
+-- Constructing function closures
+evalE g l@(Letfun b@(Bind id _ [arg] exp)) = PApp $ PVal $ \v -> 
+    let 
+        g' = E.addAll g [(arg, v), (id, (evalE g l))]
+    in evalE g' exp
 
 -- Constructing partial application closures
-evalE g (App (App (Con "Cons") (Num n)) list) = Cons n (evalE g list)
 evalE g (Prim Add) = makeIOp (+)
 evalE g (Prim Sub) = makeIOp (-)
 evalE g (Prim Mul) = makeIOp (*)
@@ -92,12 +88,7 @@ evalE g (Prim Neg) = PApp $ PVal $ \(I v1) -> I (negate v1)
 -- Applying partial application closures
 evalE g (App e1 e2) = case evalE g e1 of
                         PApp (PVal f) -> f $ evalE g e2
---evalE g (App (Prim op) b) = PApp $ PVal $ applyOp op $ evalE g b
---evalE g (App (App (Prim op) e1) e2) = applyOp op (evalE g e1) (evalE g e2)
---evalE g (App (Prim op) e2) = applyUnOp op (evalE g e2)
+                        g -> error $ "wtf?! how did I get a: " ++ show g
 
 evalE g e = error $ "Not yet handling:\n\t" ++ show e ++ "\nWith Context: \n\t" ++ show g
-
---evalE g e = error $ "Not yet handling:\n\t" ++ show e ++ "\nWith Context: \n\t" ++ show g
---evalE g (Let [(Bind id _ args (Letfun a))] inExp) = error $ "yolo: " ++ show a
 
