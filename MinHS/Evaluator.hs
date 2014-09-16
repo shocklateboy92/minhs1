3e6110d36b2b2a8bfb88d6f5c5fd69b35dac550d
Lasath Fernando <kde@lasath.org>
2014-09-16 20:08:47 +1000

    Implement partial expression application
    It's sort of like currying, with lambdas. Things need to be fixed
    up, and checked to make sure it works, but it's still progress.

--- MinHS/Evaluator.hs
+++ MinHS/Evaluator.hs
@@ -14,6 +14,7 @@ data Value = I Integer
            | Nil
            | Cons Integer Value
            | Closure VEnv Bind
+           | PApp PVal
            -- Others as needed
            deriving (Show)
 
@@ -27,29 +28,12 @@ instance PP.Pretty Value where
 evaluate :: Program -> Value
 evaluate bs = evalE E.empty (Let bs (Var "main"))
 
-applyOp :: Op -> Value -> Value -> Value
-applyOp Add (I x) (I y) = I (x + y)
-applyOp Sub (I x) (I y) = I (x - y)
-applyOp Mul (I x) (I y) = I (x * y)
-applyOp Quot (I x) (I y) = I (x `div` y)
-applyOp Rem (I x) (I y) = I (x `rem` y)
-applyOp Gt (I x) (I y) = B (x > y)
-applyOp Ge (I x) (I y) = B (x >= y)
-applyOp Lt (I x) (I y) = B (x < y)
-applyOp Le (I x) (I y) = B (x <= y)
-applyOp Eq (B x) (B y) = B (x == y)
-applyOp Ne (B x) (B y) = B (x /= y)
-applyOp Eq (I x) (I y) = B (x == y)
-applyOp Ne (I x) (I y) = B (x /= y)
-applyOp op _ _ = error $ "Illegal use of " ++ show op
+-- Helper functions for constructing partial application closures
+makeBOp :: (Integer -> Integer -> Bool) -> Value
+makeBOp f = PApp $ PVal (\(I v1) -> PApp (PVal (\(I v2) -> (B (v1 `f` v2)))))
 
-applyUnOp :: Op -> Value -> Value
-applyUnOp Head (Cons i v) = I i
-applyUnOp Tail (Cons i v) = v
-applyUnOp Null Nil = B True
-applyUnOp Null (Cons i v) = B False
-applyUnOp Neg (I i) = I (negate i)
-applyUnOp op v = error $ "Unsupported operation: " ++ show op ++ " " ++ show v
+makeIOp :: (Integer -> Integer -> Integer) -> Value
+makeIOp f = PApp $ PVal (\(I v1) -> PApp (PVal (\(I v2) -> (I (v1 `f` v2)))))
 
 evalE :: VEnv -> Exp -> Value
 
@@ -83,12 +67,37 @@ evalE g (App (Var f) exp) = case E.lookup g f of
         Just c@(Closure context b@(Bind id _ (arg:args) fexp)) -> evalE (E.addAll context [(arg, (evalE g exp)), (id, c)]) fexp
 evalE g (App (Letfun b@(Bind id _ [arg] fexp)) exp) = evalE (E.addAll g [(arg, (evalE g exp)), (id, (Closure g b))]) fexp
 
--- Other application - 
+-- Constructing partial application closures
 evalE g (App (App (Con "Cons") (Num n)) list) = Cons n (evalE g list)
-evalE g (App (App (Prim op) e1) e2) = applyOp op (evalE g e1) (evalE g e2)
-evalE g (App (Prim op) e2) = applyUnOp op (evalE g e2)
+evalE g (Prim Add) = makeIOp (+)
+evalE g (Prim Sub) = makeIOp (-)
+evalE g (Prim Mul) = makeIOp (*)
+evalE g (Prim Quot) = makeIOp div
+evalE g (Prim Rem) = makeIOp rem
+evalE g (Prim Gt) = makeBOp (>)
+evalE g (Prim Ge) = makeBOp (>=)
+evalE g (Prim Lt) = makeBOp (<)
+evalE g (Prim Le) = makeBOp (<=)
+evalE g (Prim Eq) = makeBOp (==)
+evalE g (Prim Ne) = makeBOp (/=)
+
+evalE g (Prim Head) = PApp $ PVal $ \(Cons i v) -> I i
+evalE g (Prim Tail) = PApp $ PVal $ \(Cons i v) -> v
+evalE g (Prim Null) = PApp $ PVal $ \v -> e v
+                                        where
+                                            e (Nil)      = B True
+                                            e (Cons _ _) = B False
+evalE g (Prim Neg) = PApp $ PVal $ \(I v1) -> I (negate v1)
+
+-- Applying partial application closures
+evalE g (App e1 e2) = case evalE g e1 of
+                        PApp (PVal f) -> f $ evalE g e2
+--evalE g (App (Prim op) b) = PApp $ PVal $ applyOp op $ evalE g b
+--evalE g (App (App (Prim op) e1) e2) = applyOp op (evalE g e1) (evalE g e2)
+--evalE g (App (Prim op) e2) = applyUnOp op (evalE g e2)
 
 evalE g e = error $ "Not yet handling:\n\t" ++ show e ++ "\nWith Context: \n\t" ++ show g
 
 --evalE g e = error $ "Not yet handling:\n\t" ++ show e ++ "\nWith Context: \n\t" ++ show g
 --evalE g (Let [(Bind id _ args (Letfun a))] inExp) = error $ "yolo: " ++ show a
+
